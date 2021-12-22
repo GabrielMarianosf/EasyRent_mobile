c73ddbe7ad4eddd05472f731861669faf19add80
Gabriel Mariano de Souza Freitas <gabriel.freitas@tc.df.gov.br>
2021-12-22 14:05:57 -0300

    Adicionado TypeScript

--- App.tsx
+++ App.tsx
@@ -1,20 +1,177 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+
+import Email from "./assets/icons/email";
+import Senha from "./assets/icons/senha";
+import Iris from "./assets/icons/iris";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.fundo}>
+      <View style={styles.logo}></View>
+      <View style={styles.nav}>
+        <Text
+          style={{
+            fontFamily: "ABeeZee-Regular",
+            color: "#fff",
+            fontSize: 20,
+            textShadowColor: "rgba(0, 0, 0, 0.2)",
+            textShadowOffset: { width: 5, height: 10 },
+            textShadowRadius: 10,
+            paddingBottom: 30,
+          }}
+        >
+          Sign In
+        </Text>
+        <View style={styles.view_botao}>
+          <View style={{ left: 15, top: 14 }}>
+            <Email />
+          </View>
+          <TextInput
+            style={styles.input}
+            placeholder="abc@email.com"
+          ></TextInput>
+        </View>
+        <View style={styles.view_botao}>
+          <View style={{ left: 17, top: 14 }}>
+            <Senha />
+          </View>
+          <TextInput
+            style={styles.input}
+            placeholder="Sua Senha"
+            secureTextEntry={true}
+          ></TextInput>
+        </View>
+        <Text
+          style={{
+            top: 6,
+            left: 95,
+            fontFamily: "ABeeZee-Regular",
+            color: "#fff",
+            fontSize: 15,
+            textShadowColor: "rgba(0, 0, 0, 0.2)",
+            textShadowOffset: { width: 5, height: 10 },
+            textShadowRadius: 10,
+            paddingTop: 3,
+          }}
+        >
+          Esqueceu sua Senha ?
+        </Text>
+        <Pressable style={styles.botao} onPress={null}>
+          <Text
+            style={{
+              fontFamily: "ABeeZee-Regular",
+              color: "#fff",
+              fontSize: 15,
+            }}
+          >
+            Entrar
+          </Text>
+        </Pressable>
+        <Text
+          style={{
+            top: 15,
+            left: 80,
+            fontFamily: "ABeeZee-Regular",
+            color: "#fff",
+            fontSize: 15,
+            textShadowColor: "rgba(0, 0, 0, 0.2)",
+            textShadowOffset: { width: 5, height: 10 },
+            textShadowRadius: 10,
+            paddingTop: 3,
+          }}
+        >
+          Novo Por Aqui ?
+        </Text>
+        <Pressable style={styles.botao} onPress={null}>
+          <Text
+            style={{
+              fontFamily: "ABeeZee-Regular",
+              color: "#fff",
+              fontSize: 15,
+            }}
+          >
+            Inscreva-se
+          </Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <View style={{ left: -15 }}>
+          <Iris />
+        </View>
+        <Text
+          style={{
+            top: -40,
+            left: 20,
+            fontFamily: "ABeeZee-Regular",
+            color: "#fff",
+            fontSize: 15,
+            textShadowColor: "rgba(0, 0, 0, 0.2)",
+            textShadowOffset: { width: 5, height: 10 },
+            textShadowRadius: 10,
+            paddingTop: 3,
+          }}
+        >
+          Iris Software Studio
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  fundo: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#36393F",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {},
+  fonte: {
+    fontFamily: "ABeeZee-Regular",
+    color: "#fff",
+    fontSize: 20,
+    textShadowColor: "rgba(0, 0, 0, 0.2)",
+    textShadowOffset: { width: 5, height: 10 },
+    textShadowRadius: 10,
+  },
+  input: {
+    height: 50,
+    width: 200,
+    left: 50,
+    top: -20,
+    borderRadius: 10,
+    fontFamily: "ABeeZee-Regular",
+  },
+  view_botao: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    margin: 5,
+    width: 250,
+    height: 50,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    elevation: 8,
+  },
+  botao: {
+    alignItems: "center",
+    justifyContent: "center",
+    left: 1,
+    width: 250,
+    height: 50,
+    marginTop: 30,
+    elevation: 3,
+    borderRadius: 40,
+    backgroundColor: "#0B8EB7",
+  },
+  nav: {},
+  footer: {
+    top: 80,
   },
 });
